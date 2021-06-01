b3483ec0bfb791ef618b8f192e79869ccced3c54
jackfetkovich <johnfetkovich1014@gmail.com>
2021-06-01 13:49:31 -0400

    Change styling and add fontawesome icons

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,7 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFilm } from '@fortawesome/free-solid-svg-icons'
+
+
 const Nav = () => {
   return(
     <nav>
-      <h1>Movie Search</h1>
+      <h1><FontAwesomeIcon icon={faFilm} />   Movie Search</h1>
     </nav>
   )
 }
